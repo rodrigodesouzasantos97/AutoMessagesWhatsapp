db87c0eb64096c9b826a2b568d1de1bb4df61f66
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-22 09:28:51 -0300

    feat: create service send message

---
+++ backend/src/services/sendMessage.js
@@ -0,0 +1,10 @@
+export const sendMessage = async (
+  phone,
+  message
+) => {
+  console.log("==============");
+  console.log("ENVIANDO MENSAGEM");
+  console.log("Telefone:", phone);
+  console.log("Mensagem:", message);
+  console.log("==============");
+};
