d4202b02e3e2a5f3b6cb71155b8c16cb4de6fec8
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-21 22:44:15 -0300

    fix: fix in dns error

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import dns from "dns";
 
 import app from "./src/app.js";
 
 dotenv.config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 mongoose
   .connect(process.env.MONGO_URI)
