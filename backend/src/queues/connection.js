769a582fc52e77f77902cb4c3aa79673e5505fcb
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-21 22:56:33 -0300

    fix: fix in connection

--- backend/src/queues/connection.js
+++ backend/src/queues/connection.js
@@ -3,4 +3,14 @@ import IORedis from "ioredis";
 export const connection = new IORedis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+
+  maxRetriesPerRequest: null,
+});
+
+connection.on("connect", () => {
+  console.log("Redis conectado!");
+});
+
+connection.on("error", (err) => {
+  console.log("Erro Redis:", err);
 });
