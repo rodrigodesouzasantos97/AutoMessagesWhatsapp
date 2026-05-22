f4021b20f02f758f5537abb338f7b9b706087f28
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-21 23:12:03 -0300

    refactor: add more infos about job

--- backend/src/workers/messageWorker.js
+++ backend/src/workers/messageWorker.js
@@ -5,10 +5,22 @@ import { connection } from "../queues/connection.js";
 export const worker = new Worker(
   "messages",
   async (job) => {
-    console.log("JOB RECEBIDO:");
+    console.log("==============");
+    console.log("JOB RECEBIDO");
+    console.log(job.name);
     console.log(job.data);
+    console.log("==============");
   },
   {
     connection,
   }
 );
+
+worker.on("completed", (job) => {
+  console.log(`Job ${job.id} concluído!`);
+});
+
+worker.on("failed", (job, err) => {
+  console.log(`Job ${job?.id} falhou!`);
+  console.log(err);
+});
