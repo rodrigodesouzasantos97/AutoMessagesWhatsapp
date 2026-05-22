3078c4d06ab1ce83cec43c7ac5250919cbe2470b
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-22 12:56:17 -0300

    feat: create contacts import

---
+++ backend/src/controllers/contactController.js
@@ -0,0 +1,42 @@
+import fs from "fs";
+
+import csv from "csv-parser";
+
+import Contact from "../models/Contact.js";
+
+export const importContacts = async (
+  req,
+  res
+) => {
+  try {
+    const contacts = [];
+
+    fs.createReadStream(req.file.path)
+
+      .pipe(csv())
+
+      .on("data", (row) => {
+        contacts.push({
+          name: row.nome,
+          phone: row.telefone,
+        });
+      })
+
+      .on("end", async () => {
+        await Contact.insertMany(
+          contacts
+        );
+
+        return res.status(200).json({
+          msg: "Contatos importados!",
+          total: contacts.length,
+        });
+      });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      msg: "Erro ao importar contatos",
+    });
+  }
+};
