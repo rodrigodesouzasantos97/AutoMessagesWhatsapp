795682671e9c296ddff1d7541037bcd7a2a2f10e
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-22 09:33:33 -0300

    feat: worker search data and send message

--- backend/src/workers/messageWorker.js
+++ backend/src/workers/messageWorker.js
@@ -2,25 +2,50 @@ import { Worker } from "bullmq";
 
 import { connection } from "../queues/connection.js";
 
+import Contact from "../models/Contact.js";
+import FlowStep from "../models/FlowStep.js";
+
+import { sendMessage } from "../services/sendMessage.js";
+
 export const worker = new Worker(
   "messages",
+
   async (job) => {
-    console.log("==============");
     console.log("JOB RECEBIDO");
-    console.log(job.name);
     console.log(job.data);
-    console.log("==============");
+
+    const {
+      contactId,
+      stepId,
+    } = job.data;
+
+    console.log("Buscando contato...");
+
+    const contact =
+      await Contact.findById(contactId);
+
+    console.log("Contato:", contact);
+
+    console.log("Buscando step...");
+
+    const step =
+      await FlowStep.findById(stepId);
+
+    console.log("Step:", step);
+
+    if (!contact || !step) {
+      console.log("Contato ou step não encontrados");
+
+      return;
+    }
+
+    await sendMessage(
+      contact.phone,
+      step.message
+    );
   },
+
   {
     connection,
   }
 );
-
-worker.on("completed", (job) => {
-  console.log(`Job ${job.id} concluído!`);
-});
-
-worker.on("failed", (job, err) => {
-  console.log(`Job ${job?.id} falhou!`);
-  console.log(err);
-});
