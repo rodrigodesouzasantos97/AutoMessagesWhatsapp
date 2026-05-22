1a51c2d43aa931a3bef6c8d3713ec5d4a276323f
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-22 09:34:44 -0300

    refactor: separate mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,17 @@
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+
 import dns from "dns";
 
 import app from "./src/app.js";
+
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Mongo conectado!");
+connectDB();
 
-    app.listen(process.env.PORT, () => {
-      console.log("Servidor rodando!");
-    });
-  })
-  .catch((err) => console.log(err));
+app.listen(process.env.PORT, () => {
+  console.log("Servidor rodando!");
+});
--- backend/worker.js
+++ backend/worker.js
@@ -1,3 +1,15 @@
+import dotenv from "dotenv";
+
+import dns from "dns";
+
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+connectDB();
+
 import "./src/workers/messageWorker.js";
 
 console.log("Worker iniciado!");
