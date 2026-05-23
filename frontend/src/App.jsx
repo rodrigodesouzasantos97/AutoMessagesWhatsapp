e7902c0d0fe63874b5c6860fcfc5f7ac26387fb6
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-23 09:50:12 -0300

    refactor: add Outlet from react router and add navbar in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import Contacts from "./routes/Contacts";
+import Campaigns from "./routes/Campaigns";
+import Flows from "./routes/Flows";
+import Navbar from "./components/Navbar";
+
+import { Outlet } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Contacts />
-    </>
+    <div className="app">
+      <Navbar />
+      <Outlet />
+    </div>
   );
 }
 
