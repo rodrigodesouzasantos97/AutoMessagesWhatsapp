ceded943bf9d449d24bf064634b4a73b0bcc2dfb
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-23 09:48:53 -0300

    feat: add react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,40 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/Home.jsx";
+import Contacts from "./routes/Contacts.jsx";
+import Campaigns from "./routes/Campaigns.jsx";
+import Flows from "./routes/Flows.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contacts",
+        element: <Contacts />,
+      },
+      {
+        path: "/campaigns",
+        element: <Campaigns />,
+      },
+      {
+        path: "/flows",
+        element: <Flows />,
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
