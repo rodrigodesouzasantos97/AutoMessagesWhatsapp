788144cfdf4eea28829527376567b77d80980e9f
Rodrigo Souza <rodrigodesouzasantos97@gmail.com>
2026-05-23 09:48:07 -0300

    feat: create temporary Home

---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return(
+        <div className="home">
+            <h1>Auto Messages Whatsapp</h1>
+        </div>
+    )
+}
+
+export default Home;
